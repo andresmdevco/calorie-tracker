e0fcc887b2b91159111d077b17f2cb77ae940c8d
Andrés Muñoz <andres2078@gmail.com>
2026-07-18 19:57:19 -0500

    feat: handle form submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from "react"
+import { useState, type ChangeEvent, type SubmitEvent } from "react"
 import type { Activity } from "../types";
 import { categories } from '../data/categories';
 
@@ -22,9 +22,18 @@ export default function Form() {
     const { name, calories } = activity
     return name.trim() !== '' && calories > 0
   }
+  
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    console.log('Submit...')
+  }
 
   return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+    <form 
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}  
+    >
 
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">Categoría:</label>
