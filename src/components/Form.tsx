026dfd485443c660059798a804dcd70538dd53db
Andrés Muñoz <andres2078@gmail.com>
2026-07-19 12:54:20 -0500

    feat: generate unique IDs for activities

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, type ChangeEvent, type Dispatch, type SubmitEvent } from "react"
+import { v4 as uuidv4 } from 'uuid'
 import type { Activity } from "../types";
 import { categories } from '../data/categories';
 import type { ActivityActions } from "../reducers/activity-reducer";
@@ -7,7 +8,8 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>
 }
 
-const initialState = {
+const initialState : Activity = {
+  id: uuidv4(),
   category: 1,
 	name: '',
 	calories: 0
@@ -34,7 +36,10 @@ export default function Form({dispatch} : FormProps) {
     e.preventDefault()
 
     dispatch({ type: 'save-activity', payload: {newActivity: activity} })
-    setActivity(initialState)
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
 
   return (
