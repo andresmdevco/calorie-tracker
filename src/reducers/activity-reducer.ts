e2830981ba5fcf63c2ba0b7c2826ed6a9e1ebee4
Andrés Muñoz <andres2078@gmail.com>
2026-07-20 14:47:55 -0500

    feat: add activity editing

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -19,16 +19,25 @@ export const activityReducer = (
         action : ActivityActions
     ) => {    
 
-    if(action.type === 'save-activity') {
+    if (action.type === 'save-activity') {
         // Este código maneja la lógica para actualizar el state
         
+        let updatedActivities: Activity[]
+        if (state.activeId) {
+            updatedActivities = state.activities.map( activity => activity.id === state.activeId ? action.payload.newActivity 
+            : activity )
+        } else {
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
+
         return {
             ...state,
-            activities: [...state.activities, action.payload.newActivity]
+            activities: updatedActivities,
+            activeId: ''
         }
     }
     
-    if(action.type === 'set-activeId') {
+    if (action.type === 'set-activeId') {
         return {
             ...state,
             activeId: action.payload.id
