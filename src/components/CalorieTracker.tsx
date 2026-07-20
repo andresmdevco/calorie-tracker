8a07305bffad75f3eefc16d1a1e7699d5000deac
Andrés Muñoz <andres2078@gmail.com>
2026-07-20 16:56:13 -0500

    feat: display consumed calories

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,11 +1,29 @@
-import type { Activity } from "../types"
+import { useMemo } from "react";
+import type { Activity } from "../types";
 
 type CalorieTrackerProps = {
-    activities: Activity[]
-}
+  activities: Activity[];
+};
 
-export default function CalorieTracker({activities} : CalorieTrackerProps) {
+export default function CalorieTracker({ activities }: CalorieTrackerProps) {
+  
+  // Contadores
+  const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 
+    ? total + activity.calories 
+    : total, 0), [activities])
+
   return (
-    <div>CalorieTracker</div>
-  )
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de Calorías
+      </h2>
+
+      <div className="flex flex-col item-center md:flex-row md:justify-between gap-5 mt-10">
+        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
+          Consumidas
+        </p>
+      </div>
+    </>
+  );
 }
