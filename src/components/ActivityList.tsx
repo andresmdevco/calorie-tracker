0783c134c7b24a7548af4f90b940631d2799fdb6
Andrés Muñoz <andres2078@gmail.com>
2026-07-19 20:30:03 -0500

    feat: add edit button to activity list

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import type { Activity } from "../types";
 import { categories } from '../data/categories';
+import { PencilSquareIcon } from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
   activities: Activity[]
@@ -30,8 +31,12 @@ export default function ActivityList({activities} : ActivityListProps) {
             </p>
           </div>
 
-          <div>
-
+          <div className="flex gap-5 items-center">
+            <button>
+              <PencilSquareIcon
+                className="h-8 w-8 text-gray-800"
+              />
+            </button>
           </div>
         </div>
 
